e05dd84991557fb5b5e68918043692d3fe573a15
Sam Lown <sam.lown@invopop.com>
2024-03-19 22:35:14 +0000

    Updating Test Connection

--- test_connection.go
+++ test_connection.go
@@ -1,6 +1,8 @@
 package ticketbai
 
 import (
+	"context"
+
 	"github.com/invopop/gobl.ticketbai/internal/doc"
 	"github.com/invopop/gobl/bill"
 )
@@ -13,19 +15,19 @@ type TestConnection struct {
 }
 
 // Post mocks the Post method of the Connection interface
-func (tc *TestConnection) Post(_ *bill.Invoice, _ *doc.TicketBAI) error {
+func (tc *TestConnection) Post(_ context.Context, _ *bill.Invoice, _ *doc.TicketBAI) error {
 	tc.postCalled = true
 	return nil
 }
 
 // Cancel mocks the Cancel method of the Connection interface
-func (tc *TestConnection) Cancel(_ *bill.Invoice, _ *doc.AnulaTicketBAI) error {
+func (tc *TestConnection) Cancel(_ context.Context, _ *bill.Invoice, _ *doc.AnulaTicketBAI) error {
 	tc.cancelCalled = true
 	return nil
 }
 
 // Fetch mocks the Fetch method of the Connection interface
-func (tc *TestConnection) Fetch(_ string, _ string, _ int, _ int, _ *doc.CabeceraFactura) ([]*doc.TicketBAI, error) {
+func (tc *TestConnection) Fetch(_ context.Context, _ string, _ string, _ int, _ int, _ *doc.CabeceraFactura) ([]*doc.TicketBAI, error) {
 	tc.fetchCalled = true
 	return nil, nil
 }
