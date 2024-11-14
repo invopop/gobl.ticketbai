7391dc64430a2f09fd6cab8418c9aaf296d61e88
Sam Lown <sam.lown@invopop.com>
2024-11-14 17:18:39 +0000

    Fixing test connection

--- test_connection.go
+++ test_connection.go
@@ -4,30 +4,25 @@ import (
 	"context"
 
 	"github.com/invopop/gobl.ticketbai/doc"
-	"github.com/invopop/gobl/bill"
+	"github.com/invopop/gobl.ticketbai/internal/gateways"
 )
 
 // TestConnection is a mock gateway connection for testing purposes
 type TestConnection struct {
 	postCalled   bool
-	fetchCalled  bool
 	cancelCalled bool
 }
 
+var _ gateways.Connection = (*TestConnection)(nil)
+
 // Post mocks the Post method of the Connection interface
-func (tc *TestConnection) Post(_ context.Context, _ *bill.Invoice, _ *doc.TicketBAI) error {
+func (tc *TestConnection) Post(_ context.Context, _ *doc.TicketBAI) error {
 	tc.postCalled = true
 	return nil
 }
 
 // Cancel mocks the Cancel method of the Connection interface
-func (tc *TestConnection) Cancel(_ context.Context, _ *bill.Invoice, _ *doc.AnulaTicketBAI) error {
+func (tc *TestConnection) Cancel(_ context.Context, _ *doc.AnulaTicketBAI) error {
 	tc.cancelCalled = true
 	return nil
-}
-
-// Fetch mocks the Fetch method of the Connection interface
-func (tc *TestConnection) Fetch(_ context.Context, _ string, _ string, _ int, _ int, _ *doc.CabeceraFactura) ([]*doc.TicketBAI, error) {
-	tc.fetchCalled = true
-	return nil, nil
 }
